9ce28f94b889b5e845efa7ae2c5de4715fa789c0
anil.basantani <anil@technostacks.com>
2020-06-13 11:52:14 +0530

    Initial Commit

---
+++ app/src/main/java/com/andy_dev/arpractical/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.andy_dev.arpractical
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
