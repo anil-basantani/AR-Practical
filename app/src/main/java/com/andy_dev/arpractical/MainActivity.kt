f3dfb04dd6e8d6122528d515ed579f08d431083a
anil.basantani <anil@technostacks.com>
2020-06-13 13:17:14 +0530

    Layout designing

--- app/src/main/java/com/andy_dev/arpractical/MainActivity.kt
+++ app/src/main/java/com/andy_dev/arpractical/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.andy_dev.arpractical
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.andy_dev.arpractical.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = this::class.java.simpleName
+    private lateinit var mActivity: Activity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this@MainActivity,
+            R.layout.activity_main
+        )
+        init()
+    }
+
+    private fun init() {
+        mActivity = this@MainActivity
+        supportActionBar?.title = "Facts"
     }
 }
